c781ff23b092890c80927362342cca1d04c63fa3
Ilia Scholl <ilia.scholl@up42.com>
2022-12-01 14:22:58 +0100

    Day01

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,15 +1,22 @@
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
-    }
+    fun part1(input: List<String>) = input
+        .fold(mutableListOf(mutableListOf<String>())) { acc, value ->
+            if (value.isBlank()) acc.also { it.add(mutableListOf()) }
+            else acc.also { it.last().add(value) }
+        }.maxOf { it.sumOf(String::toInt) }
 
-    fun part2(input: List<String>): Int {
-        return input.size
-    }
+    fun part2(input: List<String>) = input
+        .fold(mutableListOf(mutableListOf<String>())) { acc, value ->
+            if (value.isBlank()) acc.also { it.add(mutableListOf()) }
+            else acc.also { it.last().add(value) }
+        }.map { it.sumOf(String::toInt) }
+        .sortedDescending()
+        .take(3)
+        .sum()
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(part1(testInput) == 19)
 
     val input = readInput("Day01")
     println(part1(input))
