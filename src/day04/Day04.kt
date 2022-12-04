0d4420d85bcdf71168d0e958509891e29235f3ec
Ilia Scholl <ilia.scholl@up42.com>
2022-12-04 15:28:52 +0100

    Day04

---
+++ src/day04/Day04.kt
@@ -0,0 +1,39 @@
+@file:Suppress("ConstPropertyName")
+
+package day04
+
+import readInput
+
+private fun String.toRangePair(): Pair<IntRange, IntRange> =
+    split(',', limit = 2)
+        .map(String::toRange)
+        .let { (first, second) -> first to second }
+
+private fun String.toRange(): IntRange =
+    split('-', limit = 2)
+        .let { (start, end) -> start.toInt()..end.toInt() }
+
+private infix fun IntRange.fullyContains(other: IntRange): Boolean =
+    contains(other.first) && contains(other.last)
+
+private infix fun IntRange.overlaps(other: IntRange): Boolean =
+    this.contains(other.first) || other.contains(this.first)
+
+private
+
+fun main() {
+    fun part1(input: List<String>) = input
+        .map(String::toRangePair)
+        .count { (first, second) -> first fullyContains second || second fullyContains first }
+
+    fun part2(input: List<String>) = input
+        .map(String::toRangePair)
+        .count { (first, second) -> first overlaps second }
+
+    val testInput = readInput("day04/test")
+    check(part1(testInput) == 3)
+
+    val input = readInput("day04/input")
+    println(part1(input))
+    println(part2(input))
+}
